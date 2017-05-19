f5c6c3cd0bf09ec245565ae315ee8f048175e300
刘东辉 <liudh@9186.com>
2017-05-19 14:24:17 +0800

    多个队列

--- src/main/java/com/enh/mq/controller/TestController.java
+++ src/main/java/com/enh/mq/controller/TestController.java
@@ -26,6 +26,12 @@ public class TestController {
     private Producer producer;
     @Value("#{appConfig['mq.queue']}")
     private String queueId;
+    @Value("#{appConfig['mq.queue1']}")
+    private String queueId1;
+    @Value("#{appConfig['mq.queue2']}")
+    private String queueId2;
+    @Value("#{appConfig['mq.queue3']}")
+    private String queueId3;
 
     /**
      * @Description: 消息队列
@@ -37,8 +43,11 @@ public class TestController {
     public String testQueue() {
         try {
             Map<String, Object> map = new HashMap<String, Object>();
-            map.put("data", "hello rabbitmq");
+            map.put("data", "hello rabbitmq ");
+            Map<String, Object> maps = new HashMap<String, Object>();
+            maps.put("data", "hello rabbitmq liudh");
             producer.sendQueue(queueId + "_exchange", queueId + "_patt", map);
+            producer.sendQueue(queueId + "_exchange", "liudh_patt", maps);
         } catch (Exception e) {
             e.printStackTrace();
         }
